32ad3def918f39a9e9df8758d13b8142c2502f7c
Steffen Brem <steffenbrem@gmail.com>
2016-04-14 15:52:37 +0200

    Initial

---
+++ public/js/slider-home.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $('#layerslider').layerSlider({
+        skinsPath: 'https://s3.eu-central-1.amazonaws.com/shopblender/themes/406bd6d1/assets/layerslider/skins/'
+    });
+});
